3b1e594dee93d0968b5607d38e75e57ec801a17c
mayumiyamada <y.mayumi1011@gmail.com>
2021-04-07 11:43:54 +0900

    Revert "口コミDB"
    This reverts commit d55bb196d31e2b3188c51b1f89f48cf9ce7ea3c9.

--- db/migrate/20210406121737_create_tweets.rb
+++ db/migrate/20210406121737_create_tweets.rb
@@ -1,8 +1,7 @@
 class CreateTweets < ActiveRecord::Migration[6.0]
   def change
     create_table :tweets do |t|
-      t.string :name,               null: false
-      t.text :content,              null: false
+
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_06_121737) do
-
-  create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "content", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2021_04_02_054649) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
