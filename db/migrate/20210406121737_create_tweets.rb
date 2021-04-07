951dcf87144b4268eb1810c114312e4dc763e4cd
mayumiyamada <y.mayumi1011@gmail.com>
2021-04-07 14:20:36 +0900

    復元

--- db/migrate/20210406121737_create_tweets.rb
+++ db/migrate/20210406121737_create_tweets.rb
@@ -1,7 +1,8 @@
 class CreateTweets < ActiveRecord::Migration[6.0]
   def change
     create_table :tweets do |t|
-
+      t.string :name, null: false
+      t.text :content, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_02_054649) do
+ActiveRecord::Schema.define(version: 2021_04_06_121737) do
+
+  create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "content", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
